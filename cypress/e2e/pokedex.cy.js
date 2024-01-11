d89b9e9ccfb8f3afa998f61cae44186728307d54
Oskari Heinonen <oskari.l.heinonen@helsinki.fi>
2024-01-11 19:32:40 +0200

    try with one test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,9 +4,9 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
-  it('pokemon page can be navigated to', function () {
-    cy.visit('http://localhost:5000')
-    cy.contains('charmander').click()
-    cy.contains('blaze')
-  })
+  // it('pokemon page can be navigated to', function () {
+  //   cy.visit('http://localhost:5000')
+  //   cy.contains('charmander').click()
+  //   cy.contains('blaze')
+  // })
 })
